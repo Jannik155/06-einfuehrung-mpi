adb6a79404b056198ca7c019ad85d0a5903ae612
Jannik155 <jannikschuster39gmail.com>
2025-11-22 18:26:56 +0100

    Aufgabe 1

--- timempi.c
+++ timempi.c
@@ -5,8 +5,10 @@
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
+#include <mpi.h>
 
-int main(void) {
+int main(int argc, char *argv[])
+{
 
   struct timeval tv;
   time_t current_time;
@@ -21,11 +23,61 @@ int main(void) {
   current_time = tv.tv_sec;
   micro_sec = tv.tv_usec;
 
+  int rank;
+  int size;
+
+  MPI_Init(&argc, &argv);
+  /* Funktionen sorgen dafür, dass wir bei Verwendung von rank/size
+   * zukünftig immer prozessspezifische Werte nutzen */
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
   strftime(time_string, 30, "%Y-%m-%d %T", localtime(&current_time));
-  snprintf(output, 80, "[%d] %s // %s.%d", -1, hostname, time_string, micro_sec);
+  snprintf(output, 80, "[%d] %s // %s.%d", rank , hostname, time_string, micro_sec);
 
-  printf("%s\n", output);
-  printf("%d\n", micro_sec);
+  if (rank < size - 1 && size > 1){
+    /* Paramether:
+    * IN buf: Adresse der output-Daten (IN=lesend),
+    * IN count: Anzahl der Elemente von output,
+    * IN datatype: Datentyp von output,
+    * IN dest: Rang des Empfängers(Prozesses),
+    * IN tag: Nachrichtenerkennung,
+    * IN comm: Kommunikator */
+    MPI_Send(output, 80, MPI_CHAR, size - 1, 0, MPI_COMM_WORLD);
+  }
 
-  return 0;
+  if (size > 1 && rank == size - 1)
+  {
+    for (int sender_rank = 0; sender_rank < size - 1; sender_rank++)
+    {
+      /* Paramether:
+       * OUT buf: Adresse der output-Daten (OUT=schreibend),
+       * IN count: Anzahl der Elemente von output + 1 für \0...(\0 ist immer Ende des Strings),
+       * IN datatype: Datentyp von output,
+       * IN source: Rang des Senders(Prozesses) wird durch for-Schleife bestimmt,
+       * IN tag: Nachrichtenerkennung,
+       * IN comm: Kommunikator
+       * OUT status: Ergebnis des Empfangens */
+      MPI_Recv(output, 80, MPI_CHAR, sender_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      printf("%s\n", output);
+    }
+  }
+
+int end_after_synchronize;
+
+if(rank == size-1){
+  end_after_synchronize = 1;
 }
+
+/* Paramether:
+ * INOUT void *buffer: root sendet Signal und alle anderen empfangen Signal,
+ * IN int count: wie viele Elemente,
+ * IN MPI_Datatype datatype: Datentyp von buffer,
+ * IN int root: der Prozess-Rang, der SENDEN soll,
+ * IN MPI_Comm comm: der Kommunikator (MPI_COMM_WORLD) */
+MPI_Bcast(&end_after_synchronize, 1, MPI_INT, size - 1, MPI_COMM_WORLD);
+
+printf("[%d] beendet jetzt!\n", rank);
+MPI_Finalize();
+return 0;
+  }
